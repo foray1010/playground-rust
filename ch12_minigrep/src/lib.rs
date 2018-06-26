8e5bdc0e5f0e0d391802c1758044e188d1380eaa
foray1010 <foray1010@users.noreply.github.com>
2018-06-26 21:02:24 +0800

    refactor(ch12): follow functional programming practice

--- ch12_minigrep/src/lib.rs
+++ ch12_minigrep/src/lib.rs
@@ -40,36 +40,30 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
         search_case_insensitive(&config.query, &contents)
     };
 
-    for line in results {
-        println!("{}", line);
+    if results.len() > 0 {
+        println!("{}", results.join("\n"));
     }
 
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
+    contents.lines().fold(Vec::new(), |mut acc, x| {
+        if x.contains(query) {
+            acc.push(x);
         }
-    }
-
-    results
+        acc
+    })
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
+    contents.lines().fold(Vec::new(), |mut acc, x| {
+        if x.to_lowercase().contains(&query) {
+            acc.push(x);
         }
-    }
-
-    results
+        acc
+    })
 }
 
 #[cfg(test)]
