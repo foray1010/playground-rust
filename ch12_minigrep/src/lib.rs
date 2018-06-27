a7e7bd3541046d3dcb18cbae8383b70c87057b5e
foray1010 <foray1010@users.noreply.github.com>
2018-06-27 13:16:48 +0800

    refactor(ch12): follow functional programming practice suggest by book

--- ch12_minigrep/src/lib.rs
+++ ch12_minigrep/src/lib.rs
@@ -10,13 +10,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -48,22 +53,18 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents.lines().fold(Vec::new(), |mut acc, x| {
-        if x.contains(query) {
-            acc.push(x);
-        }
-        acc
-    })
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    contents.lines().fold(Vec::new(), |mut acc, x| {
-        if x.to_lowercase().contains(&query) {
-            acc.push(x);
-        }
-        acc
-    })
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
